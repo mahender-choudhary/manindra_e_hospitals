b455ebb7b1884ae9f5bf67ee07eeb69dc19ce2b6
Mahendra Choudhary <88625096+mahender-choudhary@users.noreply.github.com>
2022-05-18 20:30:35 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,11 @@
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    $con = mysqli_connect($server, $username, $password);
+
+    if($con){
+        die("")
+    }
+?>
